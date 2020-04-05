5e3a217e3db6b5c978ee8f4d8ea1024f2785d55a
Shafrom <shafrom@Shafroms-iMac.local>
2020-04-05 17:25:35 +0300

    some fixes

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const plumber - require('gulp-plumber')
+const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
 const pugLinter = require('gulp-pug-linter')
 const htmlValidator = require('gulp-w3c-html-validator')
